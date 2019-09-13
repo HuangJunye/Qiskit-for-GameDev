2956cbac0b8f453e3d85d57a79f85491a63688e4
Huang Junye <h.jun.ye@gmail.com>
2019-09-13 21:59:05 +0800

    add qasm

--- qiskit flask server/server/server.py
+++ qiskit flask server/server/server.py
@@ -11,16 +11,31 @@ from flask import request
 from flask import Flask
 from flask_cors import CORS
 
-from api import statevector, measurement
+from api import qasm, statevector, measurement
+
 
 app = Flask(__name__)
 CORS(app)
 
 logging.getLogger('flask_cors').level = logging.DEBUG # for debugging
 
+
 @app.route('/')
 def welcome():
     return "Hi Qiskiter!"
+
+
+@app.route('/api/run/qasm', methods=['GET'])
+def run_qasm():
+    qasm_string = request.args['qasm']
+    backend = request.args['backend']
+    print("--------------")
+    print('qasm: ', qasm_string)
+    print('backend: ', backend)
+    print("^^^^^^^^^^^^^^")
+    output = qasm(qasm_string, backend)
+    ret = {"result": output}
+    return jsonify(ret)
 
 
 @app.route('/api/run/get_statevector', methods=['POST'])
