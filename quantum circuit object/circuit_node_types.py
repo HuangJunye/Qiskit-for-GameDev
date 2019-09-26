3f4ac4bb9cc76029b70379f9bcba6cf575996491
Huang Junye <h.jun.ye@gmail.com>
2019-09-26 21:34:16 +0800

    use qasm command as the string representation of node type

---
+++ quantum circuit object/circuit_node_types.py
@@ -0,0 +1,31 @@
+#
+# Copyright 2019 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+EMPTY = 1
+CTRL = 2  # "control" part of multi-qubit gate
+TRACE = 3  # In the path between a gate part and a "control" or "swap" part
+IDEN = 'id'
+X = 'x'
+Y = 'y'
+Z = 'z'
+H = 'h'
+S = 's'
+SDG = 'sdg'
+T = 't'
+TDG = 'tdg'
+SWAP = 'swap'
+BARRIER = 'barrier'
+MEASURE_Z = 'measure'
+
