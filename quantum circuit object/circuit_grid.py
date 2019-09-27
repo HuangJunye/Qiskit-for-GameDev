8b0de8340530a7c189e205ceb0d525131d2335d8
Huang Junye <h.jun.ye@gmail.com>
2019-09-27 22:30:42 +0800

    Create qasm for circuit grid

--- quantum circuit object/circuit_grid.py
+++ quantum circuit object/circuit_grid.py
@@ -11,7 +11,12 @@ class CircuitGrid:
     def __init__(self, qubit_count, circuit_depth):
         self.qubit_count = qubit_count
         self.circuit_depth = circuit_depth
-        self.circuit_grid = np.empty((qubit_count, qubit_count), dtype=CircuitGridNode)
+        self.circuit_grid = np.empty((qubit_count, circuit_depth), dtype=CircuitGridNode)
+        # initialize empty circuit_grid
+        for depth_index in range(self.circuit_depth):
+            for qubit_index in range(self.qubit_count):
+                self.set_node(qubit_index, depth_index, CircuitGridNode(circuit_node_types.EMPTY))
+
 
     def set_node(self, qubit_index, depth_index, circuit_grid_node):
         self.circuit_grid[qubit_index][depth_index] = \
@@ -24,107 +29,37 @@ class CircuitGrid:
     def get_node(self, qubit_index, depth_index):
         return self.circuit_grid[qubit_index][depth_index]
 
-    def create_qasm_for_node(self, qubit_index):
+    def create_qasm_for_node(self, circuit_grid_node, qubit_index):
         qasm_str = ""
-
+        node_type = circuit_grid_node.node_type
         if node_type == circuit_node_types.IDEN:
             # identity gate
-            qasm_str = qasm_str + 'id q['+qubit_index+'];'
+            qasm_str = qasm_str + 'id q[' + str(qubit_index) + '];'
         elif node_type == circuit_node_types.X or \
                 node_type == circuit_node_types.Y or \
                 node_type == circuit_node_types.Z:
             # X, Y, Z gate
-            qasm_str = qasm_str + node_type + ' q[' + qubit_index + '];'
-        else node_type == circuit_node_types.H:
+            qasm_str = qasm_str + node_type + ' q[' + str(qubit_index) + '];'
+        elif node_type == circuit_node_types.H:
             # Hadamard gate
-            qasm_str = qasm_str + node_type + ' q[' + qubit_index + '];'
+            qasm_str = qasm_str + node_type + ' q[' + str(qubit_index) + '];'
 
         return qasm_str
 
-    def compute_circuit(self):
-        """
-        Compute a QuantumCircuit object based on CircuitGrid
-        :return: QuantumCircuit object
-        """
-        qr = QuantumRegister(self.qubit_count, 'q')
-        qc = QuantumCircuit(qr)
+    def create_qasm_for_circuit(self):
+        # include header
+        qasm_str = 'OPENQASM 2.0;include "qelib1.inc";'
+
+        # define quantum registers
+        qasm_str = qasm_str + 'qreg q[' + str(self.qubit_count) + '];'
+
+        # add a column of identity gates to protect simulators from an empty circuit
+        qasm_str = qasm_str+'id q;'
 
         for depth_index in range(self.circuit_depth):
             for qubit_index in range(self.qubit_count):
-                node = self.circuit_grid[qubit_index][depth_index]
-                if node:
-                    if node.node_type == node_types.IDEN:
-                        # Identity gate
-                        qc.iden(qr[qubit_index])
-                    elif node.node_type == node_types.X:
-                        if node.radians == 0:
-                            if node.ctrl_a != -1:
-                                if node.ctrl_b != -1:
-                                    # Toffoli gate
-                                    qc.ccx(qr[node.ctrl_a], qr[node.ctrl_b], qr[qubit_index])
-                                else:
-                                    # Controlled X gate
-                                    qc.cx(qr[node.ctrl_a], qr[qubit_index])
-                            else:
-                                # Pauli-X gate
-                                qc.x(qr[qubit_index])
-                        else:
-                            # Rotation around X axis
-                            qc.rx(node.radians, qr[qubit_index])
-                    elif node.node_type == node_types.Y:
-                        if node.radians == 0:
-                            if node.ctrl_a != -1:
-                                # Controlled Y gate
-                                qc.cy(qr[node.ctrl_a], qr[qubit_index])
-                            else:
-                                # Pauli-Y gate
-                                qc.y(qr[qubit_index])
-                        else:
-                            # Rotation around Y axis
-                            qc.ry(node.radians, qr[qubit_index])
-                    elif node.node_type == node_types.Z:
-                        if node.radians == 0:
-                            if node.ctrl_a != -1:
-                                # Controlled Z gate
-                                qc.cz(qr[node.ctrl_a], qr[qubit_index])
-                            else:
-                                # Pauli-Z gate
-                                qc.z(qr[qubit_index])
-                        else:
-                            if node.ctrl_a != -1:
-                                # Controlled rotation around the Z axis
-                                qc.crz(node.radians, qr[node.ctrl_a], qr[qubit_index])
-                            else:
-                                # Rotation around Z axis
-                                qc.rz(node.radians, qr[qubit_index])
-                    elif node.node_type == node_types.S:
-                        # S gate
-                        qc.s(qr[qubit_index])
-                    elif node.node_type == node_types.SDG:
-                        # S dagger gate
-                        qc.sdg(qr[qubit_index])
-                    elif node.node_type == node_types.T:
-                        # T gate
-                        qc.t(qr[qubit_index])
-                    elif node.node_type == node_types.TDG:
-                        # T dagger gate
-                        qc.tdg(qr[qubit_index])
-                    elif node.node_type == node_types.H:
-                        if node.ctrl_a != -1:
-                            # Controlled Hadamard
-                            qc.ch(qr[node.ctrl_a], qr[qubit_index])
-                        else:
-                            # Hadamard gate
-                            qc.h(qr[qubit_index])
-                    elif node.node_type == node_types.SWAP:
-                        if node.ctrl_a != -1:
-                            # Controlled Swap
-                            qc.cswap(qr[node.ctrl_a], qr[qubit_index], qr[node.swap])
-                        else:
-                            # Swap gate
-                            qc.swap(qr[qubit_index], qr[node.swap])
-
-        return qc
+                qasm_str = qasm_str + self.create_qasm_for_node(self.circuit_grid[qubit_index][depth_index], qubit_index)
+        return qasm_str
 
 
 class CircuitGridNode:
