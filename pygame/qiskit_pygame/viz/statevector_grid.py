708bb21e7ddb4bdbaa3b0d519e9bf17db60cccca
Huang Junye <h.jun.ye@gmail.com>
2019-10-03 11:51:54 +0800

    revert statevector_grid.py from QPong version to quantum_circuit_pygame version

--- pygame/qiskit_pygame/viz/statevector_grid.py
+++ pygame/qiskit_pygame/viz/statevector_grid.py
@@ -15,73 +15,45 @@
 # limitations under the License.
 #
 import pygame
-from qiskit import BasicAer, execute, ClassicalRegister
-from utils.colors import WHITE, BLACK
-from utils.fonts import VECTOR_FONT
-from utils.parameters import WIDTH_UNIT
-from utils.states import comp_basis_states
-from copy import deepcopy
-from utils.ball import Ball
+from qiskit import BasicAer, execute
+from ..utils.colors import WHITE, BLACK
+from ..utils.states import comp_basis_states
 
 
 class StatevectorGrid(pygame.sprite.Sprite):
     """Displays a statevector grid"""
-    def __init__(self, circuit, qubit_num, num_shots):
+    def __init__(self, circuit):
         pygame.sprite.Sprite.__init__(self)
         self.image = None
         self.rect = None
-        self.ball = Ball()
-        self.block_size = int(round(self.ball.screenheight / 2 ** qubit_num))
         self.basis_states = comp_basis_states(circuit.width())
-        self.circuit = circuit
+        self.set_circuit(circuit)
 
-        self.paddle = pygame.Surface([WIDTH_UNIT, self.block_size])
-        self.paddle.fill(WHITE)
-        self.paddle.convert()
+    # def update(self):
+    #     # Nothing yet
+    #     a = 1
 
-        self.paddle_before_measurement(circuit, qubit_num, num_shots)
-
-    def display_statevector(self, qubit_num):
-        for y in range(2**qubit_num):
-            text = VECTOR_FONT.render("|"+self.basis_states[y]+">", 1, WHITE)
-            text_height = text.get_height()
-            y_offset = self.block_size * 0.5 - text_height * 0.5
-            self.image.blit(text, (2 * WIDTH_UNIT, y * self.block_size + y_offset))
-
-    def paddle_before_measurement(self, circuit, qubit_num, shot_num):
-        self.update()
-        self.display_statevector(qubit_num)
-
+    def set_circuit(self, circuit):
         backend_sv_sim = BasicAer.get_backend('statevector_simulator')
-        job_sim = execute(circuit, backend_sv_sim, shots=shot_num)
+        job_sim = execute(circuit, backend_sv_sim)
         result_sim = job_sim.result()
+
         quantum_state = result_sim.get_statevector(circuit, decimals=3)
 
-        for y in range(len(quantum_state)):
-            if abs(quantum_state[y]) > 0:
-                self.paddle.set_alpha(int(round(abs(quantum_state[y])*255)))
-                self.image.blit(self.paddle, (0, y * self.block_size))
-
-    def paddle_after_measurement(self, circuit, qubit_num, shot_num):
-        self.update()
-        self.display_statevector(qubit_num)
-
-        backend_sv_sim = BasicAer.get_backend('qasm_simulator')
-        cr = ClassicalRegister(qubit_num)
-        measure_circuit = deepcopy(circuit)  # make a copy of circuit
-        measure_circuit.add_register(cr)    # add classical registers for measurement readout
-        measure_circuit.measure(measure_circuit.qregs[0], measure_circuit.cregs[0])
-        job_sim = execute(measure_circuit, backend_sv_sim, shots=shot_num)
-        result_sim = job_sim.result()
-        counts = result_sim.get_counts(circuit)
-
-        self.paddle.set_alpha(255)
-        self.image.blit(self.paddle, (0, int(list(counts.keys())[0], 2) * self.block_size))
-
-        return int(list(counts.keys())[0], 2)
-
-    def update(self):
-        self.image = pygame.Surface([(self.circuit.width() + 1) * 3 * WIDTH_UNIT, self.ball.screenheight])
+        self.image = pygame.Surface([(circuit.width() + 1) * 50, 100 + len(quantum_state) * 50])
         self.image.convert()
-        self.image.fill(BLACK)
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
+
+        block_size = 50
+        x_offset = 50
+        y_offset = 50
+        for y in range(len(quantum_state)):
+            text_surface = ARIAL_30.render(self.basis_states[y], False, (0, 0, 0))
+            self.image.blit(text_surface,(x_offset, (y + 1) * block_size + y_offset))
+            rect = pygame.Rect(x_offset + circuit.width() * 20,
+                               (y + 1) * block_size + y_offset,
+                               abs(quantum_state[y]) * block_size,
+                               abs(quantum_state[y]) * block_size)
+            if abs(quantum_state[y]) > 0:
+                pygame.draw.rect(self.image, BLACK, rect, 1)
